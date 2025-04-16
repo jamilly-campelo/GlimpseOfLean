c97e8551629c14d1473be0722d4e0a3596ae2f54
jamilly-campelo <jamillyemilly.je@gmail.com>
2025-04-16 10:49:40 -0300

    complete 02Iff

--- GlimpseOfLean/Exercises/02Iff.lean
+++ GlimpseOfLean/Exercises/02Iff.lean
@@ -35,7 +35,9 @@ prove one-by-one.
 -/
 
 example (a b : ℝ) (ha : 0 < a) (hb : 0 < b) : 0 < a^2 + b^2 := by {
-  sorry
+  apply add_pos
+  exact sq_pos_of_pos ha
+  exact sq_pos_of_pos hb
 }
 
 /-
@@ -60,7 +62,13 @@ example (a : ℝ) (ha : 0 < a) : 0 < (a^2)^2 := by {
 /- Now prove the same lemma as before using forwards reasoning. -/
 
 example (a b : ℝ) (ha : 0 < a) (hb : 0 < b) : 0 < a^2 + b^2 := by {
-  sorry
+  have h2 : 0 < a^2 := by
+    apply sq_pos_of_pos
+    exact ha
+  have h3 : 0 < b^2 := by
+    apply sq_pos_of_pos
+    exact hb
+  exact add_pos h2 h3
 }
 
 
@@ -79,7 +87,12 @@ example (a : ℝ) : a > 0 → b > 0 → a + b > 0 := by {
 /- Now prove the following simple statement in propositional logic.
 Note that `p → q → r` means `p → (q → r)`. -/
 example (p q r : Prop) : (p → q) → (p → q → r) → p → r := by {
-  sorry
+  intro h1 h2 h3
+  have q_from_p : q := h1 h3
+  apply h2 at h3
+  apply h3 at q_from_p
+  exact q_from_p
+  -- these three last lines can be replaced simply by 'exact h2 h3 q_from_p'
 }
 
 /- # Equivalences
@@ -110,7 +123,10 @@ Let's prove a variation
 -/
 
 example {a b : ℝ} (c : ℝ) : a + c ≤ b + c ↔ a ≤ b := by {
-  sorry
+  rw [← sub_nonneg]
+  have key : b + c - (a + c) = b - a := by ring
+  rw [key]
+  rw [sub_nonneg]
 }
 
 /-
@@ -148,7 +164,9 @@ example {a b : ℝ}  (ha : 0 ≤ a) : b ≤ a + b := by {
 /- Let's do a variant using `add_le_add_iff_left a : a + b ≤ a + c ↔ b ≤ c` instead. -/
 
 example (a b : ℝ) (hb : 0 ≤ b) : a ≤ a + b := by {
-  sorry
+  calc
+    a = a + 0 := by ring
+    _ ≤ a + b := by exact (add_le_add_iff_left a).2 hb
 }
 
 /-
@@ -179,7 +197,16 @@ example (a b : ℝ) : (a-b)*(a+b) = 0 ↔ a^2 = b^2 := by {
 /- You can try it yourself in this exercise. -/
 
 example (a b : ℝ) : a = b ↔ b - a = 0 := by {
-  sorry
+  constructor
+  · intro h
+    calc
+      b - a = a - a := by rw [h]
+          _ = 0     := by ring
+  · intro h
+    calc
+      a = b - (b - a) := by ring
+      _ = b - 0       := by rw [h]
+      _ = b           := by ring
 }
 
 /-
